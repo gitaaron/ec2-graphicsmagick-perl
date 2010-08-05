b3de1294b972a87c137d154b6c35feaf5d73deea
Aaron Surty <aaron.surty@gmail.com>
2010-08-05 19:37:13 -0400

    download image, convert it with gm the reupload it

--- download_convert_upload.pl
+++ download_convert_upload.pl
@@ -4,6 +4,7 @@
 # http://search.cpan.org/dist/Net-Amazon-S3/lib/Net/Amazon/S3.pm
 use strict;
 use Net::Amazon::S3;
+use Graphics::Magick;
 use Setup qw(getS3Keys);
 use lib qw(./);
 
@@ -23,7 +24,28 @@ my $bucketname = 'assets.sodalabs.com'; # @TODO change sodalabs to your domain
 
 my $bucket = $s3->add_bucket( { bucket => $bucketname } );
 
-# fetch file from the bucket
+print "Fetching file from the bucket...\n";
 my $response = $bucket->get_key_filename('assets/rooster.jpg', 'GET', 'assets/new_rooster.jpg')
     or die $s3->err . ": " . $s3->errstr;
+
+print "Doing conversion...\n";
+my $downloaded_file = './assets/new_rooster.jpg';
+my ($image, $status);
+my $image = new Graphics::Magick;
+my $status = $image->Read($downloaded_file);
+warn "$status" if "$status";
+
+annotate($image);
+
+$image->write('./assets/converted_rooster.jpg');
+
+undef $image;
+
+sub annotate {
+    my ($image) = @_;
+    my $font_file = './COOLVETI.TTF';
+    my $status = $image->Annotate(text=>"ec2 graphicsmagick perl pwned", geometry=>'5x5+350+350', font=>$font_file, pointsize=>100, fill=>'white');
+    warn "$status" if "$status";
+}
+
 
